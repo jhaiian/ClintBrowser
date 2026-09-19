748d37d6324e6ee8f05f654539d3eda17e3c67e5
jhaiian <jhaiianbetter@gmail.com>
2026-09-19 23:17:15 +0800

    Bump the version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.jhaiian.clint"
         minSdk = 26
         targetSdk = 37
-        versionCode = 30
-        versionName = "v1.1.6-beta-1"
+        versionCode = 31
+        versionName = "v1.1.6-beta-2"
     }
 
     val hasSigningConfig = localProperties.getProperty("signingConfig.storePassword") != null
